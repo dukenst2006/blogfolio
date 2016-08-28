e85098f75db13d8b8bf745e9f819fa6a54b9ea04
Clément DOUIN <clementdouin21@gmail.com>
2016-08-28 16:31:50 +0200

    Added CV picto

--- resources/views/frontend/blog/partials/header.blade.php
+++ resources/views/frontend/blog/partials/header.blade.php
@@ -12,6 +12,11 @@
             @if(isset($user->github) && strlen($user->github))
                 <a href="http://github.com/{{ $user->github }}" target="_blank" id="social"><i class="fa fa-fw fa-github"></i></a>
             @endif
+            -
+            <a href="{{ url("uploads", "clement_douin_CV_FR.pdf") }}" target="_blank" id="social" title="Curriculum Vitae (.pdf)">
+                <i class="fa fa-fw fa-file-pdf-o"></i>
+                CV
+            </a>
         </div>
     </div>
 </div>
